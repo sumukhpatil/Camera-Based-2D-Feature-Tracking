23d7a7e68840d4c8d45e9fe094087932195dc66f
sumukhpatil <spatil13@stevens.edu>
2019-11-05 16:33:13 -0500

    added HOG and Binary descriptor types

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -13,7 +13,14 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
 
     if (matcherType.compare("MAT_BF") == 0)
     {
-        int normType = cv::NORM_HAMMING;
+        int normType;
+        if (descriptorType.compare("DES_HOG") == 0) {
+          normType = cv::NORM_L2;
+        } else if (descriptorType.compare("DES_BINARY") == 0) {
+          normType = cv::NORM_HAMMING;
+        } else {
+          cout << "Incorrect descriptor type\n";
+        }
         matcher = cv::BFMatcher::create(normType, crossCheck);
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
